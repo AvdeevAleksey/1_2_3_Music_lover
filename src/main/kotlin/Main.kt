9d394d2e6965ea365c91519f0162f02d8f6ebabd
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-09 21:19:43 +0300

    It work.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    
+    val itemPrice:Int = 100
+    val itemCount:Int = 11
+    val discount:Int = 100
+    val discountStart:Int = 1000
+    val musicLover:Boolean = true
+    var totalPrice:Int = itemPrice * itemCount
+    val result = if (musicLover) {
+        val melomanPrice = totalPrice - totalPrice*0.05
+        melomanPrice - melomanPrice*0.01
+        } else if (totalPrice > discountStart) totalPrice - discount else totalPrice
+    print("Стоимость: $result")
 }
