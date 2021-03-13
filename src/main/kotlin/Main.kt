3b6c7468ce6d50f03ecef7c0eec9b67224b59089
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-13 15:08:08 +0300

    Gets the price of the music, the amount of previous purchases, and the Boolean value of whether the buyer is a music lover. 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,27 @@
 var itemPrice:Int = 0
 var itemCount:Int = 0
+const val discount:Int = 100
+const val discountStart:Int = 1000
 var musicLover:Boolean = false
 fun main() {
-    val discount:Int = 100
-    val discountStart:Int = 1000
     takeMyData()
     var totalPrice:Int = itemPrice * itemCount
-    val result = if (musicLover) {
-        val melomanPrice = totalPrice - totalPrice*0.05
-        melomanPrice - melomanPrice*0.01
-        } else if (totalPrice > discountStart) totalPrice - discount else totalPrice
-    print("Стоимость: $result")
+    burnIt(itemPrice,totalPrice,musicLover)
 }
 
 fun takeMyData() {
     print("Введите цену за музыкальный трек: ")
     itemPrice = readLine()!!.toInt()
-    print("Введите сумму предыдущих покупок: ")
-    itemCount = readLine()!!.toInt()/itemPrice
+    print("Введите количество купленых треков: ")
+    itemCount = readLine()!!.toInt()
     print("Введите 1 если Вы наш меломан и 0 если нет: ")
     musicLover = if (readLine()!!.toInt()==1) {true} else {false}
+}
+
+fun burnIt (itemPrice:Int, totalPrice:Int, musicLover:Boolean) {
+    val result = if (musicLover) {
+        val melomanPrice = totalPrice - totalPrice*0.05
+        melomanPrice - melomanPrice*0.01
+    } else if (totalPrice > discountStart) totalPrice - discount else totalPrice
+    print("Стоимость: $result")
 }
