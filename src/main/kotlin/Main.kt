76ead0a8e406a428b3061b172939844d25ca305b
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-15 20:12:10 +0300

    I corrected the code according to your comments. But this is not accurate).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 var itemPrice:Int = 0
 var itemCount:Int = 0
+var amountOfPreviousPurchases = 0
 const val discount:Int = 100
 const val discountStart:Int = 1000
 var musicLover:Boolean = false
+
 fun main() {
     takeMyData()
-    var totalPrice:Int = itemPrice * itemCount
-    burnIt(itemPrice,totalPrice,musicLover)
+    val totalPrice:Int = itemPrice * itemCount
+    generalDiscountCalculation(amountOfPreviousPurchases,totalPrice,musicLover)
 }
 
 fun takeMyData() {
@@ -14,18 +16,27 @@ fun takeMyData() {
     itemPrice = readLine()!!.toInt()
     print("Введите количество купленых треков: ")
     itemCount = readLine()!!.toInt()
+    print("Ведите сумму предыдущих покупок: ")
+    amountOfPreviousPurchases = readLine()!!.toInt()
     print("Введите 1 если Вы наш меломан и 0 если нет: ")
     musicLover = if (readLine()!!.toInt()==1) {true} else {false}
 }
 
-fun burnIt (itemPrice:Int, totalPrice:Int, musicLover:Boolean) {
-    val result = if (musicLover) {
-        val melomanPrice = totalPrice - totalPrice*0.05
-        if (melomanPrice > discountStart) {
-            melomanPrice - melomanPrice*0.01 - discount
-        } else {
-            melomanPrice - melomanPrice*0.01
-        }
-    } else if (totalPrice > discountStart) totalPrice - discount else totalPrice
+fun generalDiscountCalculation (amountOfPreviousPurchases:Int, totalPrice:Int, musicLover:Boolean) {
+    val result = if (amountOfPreviousPurchases < discountStart) {
+        melomanDiscountCalculation(totalPrice,musicLover)
+    } else if (amountOfPreviousPurchases < discountStart*10) {
+        melomanDiscountCalculation((totalPrice - discount),musicLover)
+    } else {
+        melomanDiscountCalculation((totalPrice*0.05).toInt(),musicLover)
+    }
     print("Стоимость: $result")
+}
+
+fun melomanDiscountCalculation(totalPrice:Int, musicLover:Boolean): Int {
+    return if (musicLover) {
+        (totalPrice - totalPrice * 0.01).toInt()
+    } else {
+        totalPrice
+    }
 }
